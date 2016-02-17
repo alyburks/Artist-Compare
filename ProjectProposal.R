992d562511b3b564cccddd8658877e111f941db4
Alyxis Burks <alyxistarynburks@gmail.com>
2016-02-17 15:48:08 -0800

    Set up Proposal

---
+++ ProjectProposal.R
@@ -0,0 +1,32 @@
+##Project Proposal
+
+#Project Description
+
+~~This is a written, non-technical description of your project.  Depending on the specifics of your project, you should outline the answers to these (and perhaps other) questions:~~
+  
+. What is the dataset you'll be working with?  Please include background on who collected the data, where you accessed it, and any additional information we should know about how this data came to be.
+. Who is your target audience?  Depending on the domain of your data, there may be a variety of audiences interested in using the dataset.  You should hone in on one of these audiences.
+. What does your audience want to learn from your data?  Please list out at least 3 specific questions that your project will answer for your audience.
+
+
+#Technical Description
+
+~~This section of your proposal is an opportunity to think through the specific analytical steps you'll need to complete throughout the project.~~
+  
+. What will be the format of your final product (Shiny app, HTML page or slideshow compiled with KnitR, etc.)?
+. How will you be reading in your data (i.e., are you using an API, or is it a static .csv/.json file)?
+. What types of data-wrangling (reshaping, reformatting, etc.) will you need to do to your data?
+. What (major/new) libraries will be using in this project (no need to list common libraries that are used in many projects such as dplyr)
+. What questions, if any, will you be answering with statistical analysis/machine learning?
+. What major challenges do you anticipate? 
+
+
+#Project Set-up
+
+~~In addition to outlining the steps you'll be taking to complete the project, you'll also be setting up your project on GitHub by doing the following:~~
+  
+. Create a public repository on GitHub in which you'll be completing your final projects.  All group members should be added to the repo so they can push directly to it
+. Create at least 5 GitHub Issues as your first set of steps to take in the project.  You should assign these to individual group members to complete 
+. Optional: Create a private slack channel(or a new channel) for your group to use for communication.  An awesome part of slack is you can really easily (Links to an external site.) configure it to automatically post to the channel anytime someone pushes to your GitHub repository.  
+
+For your submission, please upload a document (or multiple documents) containing your project and technical descriptions.  Please also submit your website URL of the GitHub repository you'll be using for you final project.
