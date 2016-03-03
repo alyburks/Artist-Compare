4212178c740f4cdb77d99f92f09424db457856dc
alyburks <lexyburks@gmail.com>
2016-03-03 15:30:15 -0800

    Update visualization.R

--- Artist-Compare/visualization.R
+++ Artist-Compare/visualization.R
@@ -1,1 +1,11 @@
 ##Visualization. Alyxis will do this once we talk about it.
+
+visualization1 <- function(df){
+  #Detail about bar graph
+  p <- barplot(df,
+               xlab = "Followers" ,
+               ylab = "Total Followers",
+               col = ("blue"),
+               main = 'Artist 1 Followers VS. Artist 2 Followers')
+  return(p)
+}
