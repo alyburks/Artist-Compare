9d09b326dd98fb8f635551c2ecfffb0295110363
alyburks <lexyburks@gmail.com>
2016-03-03 15:42:45 -0800

    Update data_from_artist.R

--- Artist-Compare/data_from_artist.R
+++ Artist-Compare/data_from_artist.R
@@ -2,6 +2,8 @@
 ##This is going to be the file where we collect the data we feel is necessary for use to determine who the better
 ##Artist is. Kind of like the super function from assignment 7. So since Jeff you did this orginally you can do this part
 ## Followers, Popularity, how many albums, top tracks
+##alyxis needs a data table that has the followers
+
 each_artist <- function(artist){
   artist_data <- paste0('https://api.spotify.com/v1/search?q=', artist , '&type=artist')
   artist_album <- paste0('https://api.spotify.com/v1/artists/', artist_data$id, '/albums?market=ES&album_type=album')
