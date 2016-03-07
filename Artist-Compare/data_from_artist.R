d2b99aef8e2448c79edfe7486e8dd06ba7a2b266
Alyxis Burks <aly@uw.edu?>
2016-03-07 15:51:19 -0800

    fixed readme

--- Artist-Compare/data_from_artist.R
+++ Artist-Compare/data_from_artist.R
@@ -1,16 +1,4 @@
-<<<<<<< 40176b05537d472f3a03b7b571722d188c3d09f9
-##File for data
-##This is going to be the file where we collect the data we feel is necessary for use to determine who the better
-##Artist is. Kind of like the super function from assignment 7. So since Jeff you did this orginally you can do this part
-## Followers, Popularity, how many albums, top tracks
-##alyxis needs a data table that has the followers
 
-each_artist <- function(artist){
-  artist_data <- paste0('https://api.spotify.com/v1/search?q=', artist , '&type=artist')
-  artist_album <- paste0('https://api.spotify.com/v1/artists/', artist_data$id, '/albums?market=ES&album_type=album')
-  ## top tracks https://api.spotify.com/v1/artists/{id}/top-tracks
-}
-=======
 ##File for data
 ##This is going to be the file where we collect the data we feel is necessary for use to determine who the better
 ##Artist is. Kind of like the super function from assignment 7. So since Jeff you did this orginally you can do this part
@@ -20,4 +8,4 @@ each_artist <- function(artist){
   artist_album <- paste0('https://api.spotify.com/v1/artists/', artist_data$id, '/albums?market=ES&album_type=album')
   ## top tracks https://api.spotify.com/v1/artists/{id}/top-tracks
 }
->>>>>>> whats the problem
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<<<<<<< 40176b05537d472f3a03b7b571722d188c3d09f9
+
 #Artist Compare
 
 ##INFO 498 FD
@@ -6,10 +6,3 @@
 ######Created by Alyxis Burks, Samuel Tompkins, Evan Taylor, and Jeffrey Pinkstaff
 
 This application uses shiny to request two artists from the user, prints out the data necessary to deteremine which artist is better and then returns a picture of the artist and which one is better based on the data we collected and ran through an equation.
-=======
-# Final Project Proposal
-
-## INFO 498 FD
-
-**Alyxis Burks, Samuel Tompkins, Evan Taylor, and Jeffrey Pinkstaff**
->>>>>>> whats the problem
